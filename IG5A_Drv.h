77015942c8bfec74f6a1d94812c3d8550c0e783a
BartoszSmierciak <bartosz.smierciak@gmail.com>
2021-09-02 12:22:30 +0200

    rename

---
+++ IG5A_Drv.h
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include <modbus/modbus.h>
+
+void Drv_Get_FrequencyCommand(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_AccelTime(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_DecelTime(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_DriveMode(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_FrequencySettingMethod(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_MultiStepFreq1(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_MultiStepFreq2(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_MultiStepFreq3(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_OutputCurrent(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_MotorRPM(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_InverterDCLinkVoltage(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_UserDisplaySelect(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_FaultDisplay(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_DirectionOfMotorRotationSelect(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_DriveMode2(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_FrequencySettingMethod2(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_PIDControlStandardValueSetting(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Get_PIDControlFeedbackAmount(modbus_t *ctx, uint16_t* value, int* status);
+
+
+void Drv_Set_FrequencyCommand(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_AccelTime(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_DecelTime(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_DriveMode(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_FrequencySettingMethod(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_MultiStepFreq1(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_MultiStepFreq2(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_MultiStepFreq3(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_OutputCurrent(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_MotorRPM(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_InverterDCLinkVoltage(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_UserDisplaySelect(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_FaultDisplay(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_DirectionOfMotorRotationSelect(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_DriveMode2(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_FrequencySettingMethod2(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_PIDControlStandardValueSetting(modbus_t *ctx, uint16_t* value, int* status);
+void Drv_Set_PIDControlFeedbackAmount(modbus_t *ctx, uint16_t* value, int* status);
--- ig5a_reg.h
+++
@@ -1,8 +0,0 @@
-
-struct Reg_t;
-
-struct Reg_t* Reg_new();
-
-void Rec_ctor(struct Reg_t*);
-
-void Rec_dtor(struct Reg_t*);
